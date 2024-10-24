40d4f1fab731a2025f8204c7b59d52ce79b2680d
Ghassen Guesmi <112974182+GuesmiGhassen@users.noreply.github.com>
2024-10-24 10:43:38 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,12 +1,3 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-} 
-
 def deployApp() {
     echo 'deploying the application...'
 } 
